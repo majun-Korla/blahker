9f26e555db6d66c61374a2d7ea020e037db7b44e
mason <majinmajun@gmail.com>
2024-09-09 09:46:16 +0800

    #2 swift injection

--- Features/Sources/Features/AppFeature.swift
+++ Features/Sources/Features/AppFeature.swift
@@ -1,12 +1,14 @@
-import SwiftUI
 import ComposableArchitecture
+import SwiftUI
 
 @main
 
 struct BlahkuerApp: App {
     var body: some Scene {
         WindowGroup {
-            Text("TCA ")
+            AppView(store: Store(initialState: AppFeature.State(), reducer: {
+                AppFeature()
+            }))
         }
     }
 }
@@ -14,10 +16,51 @@ struct BlahkuerApp: App {
 @Reducer
 struct AppFeature {
     struct State: Equatable {}
+
+    enum Action: Equatable {
+        case scenePhaseBecomeActive
+        case checkUserEnableContentBlocker
+        case userEnableContentBlocker(Bool)
+    }
+
+    @Dependency(\.safariService) var safariService
     
-    enum Action: Equatable {}
-    
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {}
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .scenePhaseBecomeActive:
+            return .send(.checkUserEnableContentBlocker)
+        
+        case .checkUserEnableContentBlocker:
+            return .run { send in
+                let extensionID = "com.elaborapp.Blahker.ContentBlocker"
+
+                let isEnabled = await safariService.checkUserEnableContenBloacker(extensionID)
+                await send(.userEnableContentBlocker(isEnabled))
+            }
+        
+        case .userEnableContentBlocker(let bool):
+            return .none
+        }
+    }
 }
 
+struct AppView: View {
+    let store: StoreOf<AppFeature>
+    @Environment(\.scenePhase) private var scenePhase
 
+    var body: some View {
+        Text("App View")
+            .onChange(of: scenePhase) { phase in
+                switch phase {
+                case .active:
+                    store.send(.scenePhaseBecomeActive)
+
+                case .background, .inactive:
+                    break
+
+                @unknown default:
+                    break
+                }
+            }
+    }
+}
--- Features/Tests/FeaturesTests/AppFeatureTests.swift
+++ Features/Tests/FeaturesTests/AppFeatureTests.swift
@@ -4,7 +4,46 @@ import XCTest
 
 @MainActor
 final class AppFeatureTests: XCTestCase {
-    func testAppLaunch() async throws {
+    func testAppLaunch_userHaventEnableContentBlocker() async throws {
         let store = TestStore(initialState: AppFeature.State(), reducer: { AppFeature() })
+        {
+            $0.safariService.checkUserEnableContenBloacker = { _ in false }
+        }
+        
+        await store.send(.scenePhaseBecomeActive)
+        await store.receive(.checkUserEnableContentBlocker)
+        await store.receive(.userEnableContentBlocker(false))
+        
     }
+    
+    func testAppLaunch_userAlreadyEnableContentBlocker() async throws {
+        let store = TestStore(initialState: AppFeature.State(), reducer: { AppFeature() })
+        {
+            $0.safariService.checkUserEnableContenBloacker = { _ in true }
+
+        }
+        
+        await store.send(.scenePhaseBecomeActive)
+        await store.receive(.checkUserEnableContentBlocker)
+        await store.receive(.userEnableContentBlocker(true))
+        
+    }
+    
+    func testAppLaunch_userHaventEnableContentBlocker_laterEnabled() async throws {
+        let store = TestStore(initialState: AppFeature.State(), reducer: { AppFeature() })
+        {
+            $0.safariService.checkUserEnableContenBloacker = { _ in false }
+        }
+        
+        await store.send(.scenePhaseBecomeActive)
+        await store.receive(.checkUserEnableContentBlocker)
+        await store.receive(.userEnableContentBlocker(false))
+        
+        store.dependencies.safariService.checkUserEnableContenBloacker = { _ in true }
+        await store.send(.scenePhaseBecomeActive)
+        await store.receive(.checkUserEnableContentBlocker)
+        await store.receive(.userEnableContentBlocker(true))
+        
+    }
+    
 }
